d358e78e2e0e9a5aeca220871cff2ea7f4170655
flavio02 (NixOS) <flavyjala@hotmail.com>
2025-02-26 02:11:55 -0300

    fix: added some settings for deploy

--- Asistencia/settings.py
+++ Asistencia/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Deploy
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
