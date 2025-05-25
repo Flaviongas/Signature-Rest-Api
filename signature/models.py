c012c05dd38101fdb66f05286ea1112c766c8b3c
flavio02 (NixOS) <flavyjala@hotmail.com>
2025-05-25 00:53:21 -0400

    feat: created major code table, related it to majors

--- signature/models.py
+++ signature/models.py
@@ -4,9 +4,19 @@ from django.contrib.auth.models import AbstractUser, UserManager
 # Create your models here.
 
 
+class MajorCode(models.Model):
+    # Las carreras tienen más de un código por alguna razón, culpen a la U
+    code = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.code
+
+
 class Major(models.Model):
-    name = models.CharField(max_length=200)
-    faculty = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
+    faculty = models.CharField(max_length=50)
+    code = models.ForeignKey(
+        MajorCode, on_delete=models.CASCADE, related_name='majors', null=True, default=None)
 
     def __str__(self):
         return self.name
@@ -41,25 +51,25 @@ class CustomUserManager(UserManager):
         """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        
+
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        
+
         # Crear el superusuario sin asignar majors por ahora
         user = self.create_user(username, email, password, **extra_fields)
-        
+
         # Asignar todas las carreras después de crear el usuario
         all_majors = Major.objects.all()
         user.majors.set(all_majors)
-        
+
         return user
 
 
 class PermissionUser(AbstractUser):
     majors = models.ManyToManyField(Major, related_name='users')
-    
+
     objects = CustomUserManager()
 
     def __str__(self):
